4a9ff8fbfd7ec786b13448e548bcd84f6907e472
Curiger Sandra <bjsvwcur@srsofaioi19445.verw.rootso.org>
2020-10-26 14:36:30 +0100

    new files

--- development_dbs/ili2pg_dataexortEdit_hoheitsgrenzen.sh
+++ development_dbs/ili2pg_dataexortEdit_hoheitsgrenzen.sh
@@ -1,4 +1,4 @@
-java -jar /home/bjsvwcur/ilis/ili2pg-4.3.1/ili2pg.jar --export --dbschema agi_hoheitsgrenzen_pub --models SO_Hoheitsgrenzen_Publikation_20170626 \
---dbhost geodb.rootso.org --dbport 5432 --dbdatabase edit --dbusr bjsvwcur --dbpwd IwadH61i4564O \
+java -jar /home/bjsvwcur/ilis/ili2pg-3.11.2/ili2pg.jar --export --dbschema agi_hoheitsgrenzen_pub --models SO_Hoheitsgrenzen_Publikation_20170626 \
+--dbhost geodb-i.rootso.org --dbport 5432 --dbdatabase edit --dbusr bjsvwcur --dbpwd IwadH61i4564O \
 --disableAreaValidation --disableValidation \
-agi_hoheitsgrenzen_pub_edit.xtf
+agi_hoheitsgrenzen_pub.xtf
